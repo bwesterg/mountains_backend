c782766c65f99ac468ecb0a2fb27ab772d5e99ed
bwesterg <benton.westergaard@gmail.com>
2021-08-04 13:34:11 -0600

    Basic rails setup, migration, seeds, cors, index route for Mountain.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Mountain.destroy_all
+
+Mountain.create([
+    {
+        name: "Matterhorn",
+        country: "Switzerland",
+        content: "description here",
+        elevation: 14300,
+        image1: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.outsideonline.com%2Foutdoor-adventure%2Fenvironment%2Fclimate-change-matterhorn-climbing-dangerous%2F&psig=AOvVaw1lKvozNWeOMMBdC4o5wdU-&ust=1628191305066000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCxxr6LmPICFQAAAAAdAAAAABAJ",
+        image2: "",
+        image3: "",
+    },
+    {
+        name: "Eiger",
+        country: "Switzerland",
+        content: "description here",
+        elevation: 13900,
+        image1: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEiger&psig=AOvVaw0wwlramjLlVyLrg4lRQXRy&ust=1628191418285000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDxw-yLmPICFQAAAAAdAAAAABAD",
+        image2: "",
+        image3: "",
+    },
+    {
+        name: "Piz Buin",
+        country: "Switzerland",
+        content: "description here",
+        elevation: 12500,
+        image1: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.shutterstock.com%2Fimage-photo%2Fpiz-buin-neighbors-swiss-alps-260nw-526894501.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fpiz%2Bbuin&tbnid=HXvOXp6AwBKMtM&vet=12ahUKEwjk0ZyFjJjyAhUQMqwKHWNvBk4QMygVegUIARDvAw..i&docid=AhahTsbIxUSh1M&w=396&h=280&q=piz%20buin&ved=2ahUKEwjk0ZyFjJjyAhUQMqwKHWNvBk4QMygVegUIARDvAw",
+        image2: "",
+        image3: "",
+    },
+
+
+])
